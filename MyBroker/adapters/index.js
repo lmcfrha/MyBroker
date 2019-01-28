a75a81a00f1787a6945102acd6df5a9e5ed8d2de
lmcfrha <frederic.hanslik@ericsson.com>
2019-01-28 12:13:41 -0500

    Initial DB manipulation. Nice.

--- MyBroker/adapters/index.js
+++ MyBroker/adapters/index.js
@@ -5,10 +5,96 @@
  */
 const config_mysql = require('../config/appConfig.json')['mysql']
 var mysql      = require('mysql');
-exports.dbconnection = mysql.createConnection({
+dbconnection = mysql.createConnection({
   host     : `${config_mysql.host}`,
   port     : `${config_mysql.port}`,
   user     : `${config_mysql.user}`,
-  password : `${config_mysql.password}`,
-  database : `${config_mysql.database}`
+  password : `${config_mysql.password}`
 });
+
+var dbname = `${config_mysql.database}`;
+var dbstep = 0;
+var dbready = false;
+ 
+/** dbstep : basically odd is good, even is bad
+ *  0 = Initiate mysql connection 
+ *  1 = Connect to database
+ *  2 = Create Database if DB connection fails
+ *  3 = Read tables
+ *  4 = Create tables
+ *  */
+
+
+function dbload(step,connection) {
+	switch(step) {
+	case 0: /* Connect to mysql server */
+		connection.connect(function(err) {
+			console.log('-----------Entering STEP '+step)
+			  if (err) {
+				    console.error('Error connecting to db server: ' + err.message);
+				    console.error('... go troubleshoot the db server connection yourself');
+				    
+		      } else { 
+		    	  	console.log('Connected to db server as id ' + connection.threadId);
+		    	  	dbstep = 1;
+		    	  	dbload(dbstep,connection);
+		      }
+		      });
+		break;
+	case 1: /* Connect to DB */
+		console.log('-----------Entering STEP '+step)
+		connection.changeUser({database : dbname}, function (err, result) {
+		    if (err) {
+		    	 console.log('Error connecting to database: ' + err.message);
+		    	 console.log ('...will try to create the database.');
+		    	 dbstep = 2;
+				 dbload(dbstep,connection);
+		    } else {
+		    	 console.log("Connected to database "+ dbname + "; proceed to read tables.");
+		    	 dbstep = dbstep + 2;
+		    	 dbload(dbstep,connection);
+		    }
+		    });
+		break;
+	case 2: /* Create to DB, assuming it's a new installation */
+		console.log('-----------Entering STEP '+step)
+		console.log('recreate the sql server connection before')
+		dbconnection = mysql.createConnection({
+						host     : `${config_mysql.host}`,
+						port     : `${config_mysql.port}`,
+						user     : `${config_mysql.user}`,
+						password : `${config_mysql.password}`
+		});
+		dbconnection.query("CREATE DATABASE "+dbname, function (err, result) {
+		    if (err) {
+		    	 console.error('Error creating DB: ' + err.message);
+		    	 console.log ('...go troubleshoot yourself.');
+		    } else {
+		    	 console.log("Created "+ dbname + "; proceed to create tables.");
+		    	 dbstep = 3;
+		    	 dbload(dbstep,dbconnection);
+		    }
+		  });
+		break;
+	case 3: /* Read tables */
+		console.log('-----------Entering STEP '+step)
+		console.log("Read tables.");
+		
+	}
+	
+	
+}
+
+dbload(dbstep,dbconnection); /* initiate the DB loading or creation if needed */
+
+/* dbconnection.connect(function(err) {
+	  if (err) {
+		    console.error('error connecting: ' + err.stack);
+		    return;
+		  }
+		 
+		  console.log('connected as id ' + connection.threadId);
+		});
+*/
+exports.dbconnection=dbconnection;
+
--- MyBroker/app.js
+++ MyBroker/app.js
@@ -12,14 +12,6 @@ var express = require('express')
   , auth = require('./auth');
 
 
-adapters.dbconnection.connect();
-adapters.dbconnection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-	  if (error) throw error;
-	  console.log('The solution is: ', results[0].solution);
-	}); 
-adapters.dbconnection.end();
-
-
 var app = express();
 
 // all environments
