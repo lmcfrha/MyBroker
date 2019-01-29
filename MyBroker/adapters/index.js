7817a2dae8b190fce02224297a622d657c3ee546
lmcfrha <frederic.hanslik@ericsson.com>
2019-01-28 19:56:12 -0500

    Reading DB

--- MyBroker/adapters/index.js
+++ MyBroker/adapters/index.js
@@ -5,6 +5,7 @@
  */
 const config_mysql = require('../config/appConfig.json')['mysql']
 const config_createtables = require('../config/appConfig.json')['tabledefinitions']
+const config_readtables = require('../config/appConfig.json')['fetchtables']
 
 var mysql      = require('mysql');
 
@@ -17,6 +18,7 @@ dbconnection = mysql.createConnection({
 
 var dbname = `${config_mysql.database}`;
 var dbstep = 0;
+var profiles, tickers;
 var dbready = false;
  
 /** dbstep : basically odd is good, even is bad
@@ -54,7 +56,7 @@ function dbload(step,connection) {
 				 dbload(dbstep,connection);
 		    } else {
 		    	 console.log("Connected to database "+ dbname + "; proceed to read tables.");
-		    	 dbstep = 3;
+		    	 dbstep = 3.1;
 		    	 dbload(dbstep,connection);
 		    }
 		    });
@@ -79,30 +81,50 @@ function dbload(step,connection) {
 		    }
 		  });
 		break;
-	case 3: /* Read tables */
+	case 3.1: /* Read table profile */
 		console.log('-----------Entering STEP '+step)
-		console.log("Read tables.");
-		break;
-	case 4: /* Create Table Profile */
-		console.log('-----------Entering STEP '+step)
-		console.log("Create PROFILES table: " + `${config_createtables.profile}`);
-		connection.query(`${config_createtables.profile}`, function (err, results, fields) {
+		console.log("Read PROFILE table.");
+		connection.query(`${config_readtables.profile}`, function (err, results, fields) {
 			  // error will be an Error if one occurred during the query
 			  // results will contain the results of the query
 			  // fields will contain information about the returned results fields (if any)
 		    if (err) {
-		    	 console.error('Error creating table: ' + err.message);
+		    	 console.error('Error reading table: ' + err.message);
 		    	 console.log ('...go troubleshoot yourself.');
 		    } else {
-		    	dbstep = 4.1;
+		    	profiles = results;
+		    	console.log(profiles);
+		    	dbstep = 3.2;
 		   		dbload(dbstep,connection);
 		    }
 		   });
-		break;
-	case 4.1: /* Create Table Ticker */
+		break;		
+	case 3.2: /* Read table tickers */
 		console.log('-----------Entering STEP '+step)
-		console.log("Create TICKERS table: " + `${config_createtables.ticker}`);
-		connection.query(`${config_createtables.ticker}`, function (err, results, fields) {
+		console.log("Read TICKER table.");
+		connection.query(`${config_readtables.ticker}`, function (err, results, fields) {
+			  // error will be an Error if one occurred during the query
+			  // results will contain the results of the query
+			  // fields will contain information about the returned results fields (if any)
+		    if (err) {
+		    	 console.error('Error reading table: ' + err.message);
+		    	 console.log ('...go troubleshoot yourself.');
+		    } else {
+		    	tickers = results;
+		        dbready = true;
+		    	console.log("DATABASE READY - TICKERS and PROFILES are loaded:");
+		    	console.log("--- PROFILES --- \n"+ JSON.stringify(profiles, null, ' '));
+		    	console.log("--- TICKERS --- \n"+ JSON.stringify(tickers, null, ' '));
+		/*    	console.log(tickers[1].exchange); */
+		    }
+		   });
+		break;		
+
+/*		break; */
+	case 4.1: /* Create Table Profile */
+		console.log('-----------Entering STEP '+step)
+		console.log("Create PROFILES table: " + `${config_createtables.profile}`);
+		connection.query(`${config_createtables.profile}`, function (err, results, fields) {
 			  // error will be an Error if one occurred during the query
 			  // results will contain the results of the query
 			  // fields will contain information about the returned results fields (if any)
@@ -115,10 +137,10 @@ function dbload(step,connection) {
 		    }
 		   });
 		break;
-	case 4.2: /* Create Table User */
+	case 4.2: /* Create Table Ticker */
 		console.log('-----------Entering STEP '+step)
-		console.log("Create USERS table: " + `${config_createtables.user}`);
-		connection.query(`${config_createtables.user}`, function (err, results, fields) {
+		console.log("Create TICKERS table: " + `${config_createtables.ticker}`);
+		connection.query(`${config_createtables.ticker}`, function (err, results, fields) {
 			  // error will be an Error if one occurred during the query
 			  // results will contain the results of the query
 			  // fields will contain information about the returned results fields (if any)
@@ -131,10 +153,10 @@ function dbload(step,connection) {
 		    }
 		   });
 		break;
-	case 4.3: /* Create Table Account */
+	case 4.3: /* Create Table User */
 		console.log('-----------Entering STEP '+step)
-		console.log("Create ACCOUNTS table: " + `${config_createtables.account}`);
-		connection.query(`${config_createtables.account}`, function (err, results, fields) {
+		console.log("Create USERS table: " + `${config_createtables.user}`);
+		connection.query(`${config_createtables.user}`, function (err, results, fields) {
 			  // error will be an Error if one occurred during the query
 			  // results will contain the results of the query
 			  // fields will contain information about the returned results fields (if any)
@@ -149,6 +171,22 @@ function dbload(step,connection) {
 		break;
 	case 4.4: /* Create Table Account */
 		console.log('-----------Entering STEP '+step)
+		console.log("Create ACCOUNTS table: " + `${config_createtables.account}`);
+		connection.query(`${config_createtables.account}`, function (err, results, fields) {
+			  // error will be an Error if one occurred during the query
+			  // results will contain the results of the query
+			  // fields will contain information about the returned results fields (if any)
+		    if (err) {
+		    	 console.error('Error creating table: ' + err.message);
+		    	 console.log ('...go troubleshoot yourself.');
+		    } else {
+		    	dbstep = 4.5;
+		   		dbload(dbstep,connection);
+		    }
+		   });
+		break;
+	case 4.5: /* Create Table Account */
+		console.log('-----------Entering STEP '+step)
 		console.log("Create TRANSACTIONLOGS table: " + `${config_createtables.transactionlog}`);
 		connection.query(`${config_createtables.transactionlog}`, function (err, results, fields) {
 			  // error will be an Error if one occurred during the query
@@ -158,8 +196,8 @@ function dbload(step,connection) {
 		    	 console.error('Error creating table: ' + err.message);
 		    	 console.log ('...go troubleshoot yourself.');
 		    } else {
-		    	dbstep = 3;
-		   		dbload(dbstep,connection);
+		        dbready = true;
+		    	console.log("DATABASE READY - TICKERS AND PROFILES TABLES ARE EMPTY");
 		    }
 		   });
 		break;
